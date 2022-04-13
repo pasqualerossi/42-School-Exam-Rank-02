b13319053c6d38fc29b9cd2e35487019b322c53e
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-04-13 11:10:35 +0930

    Add files via upload

---
+++ level01/first_word/first_word.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int		main(int argc, char **argv)
+{
+	int		i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i++;
+		while (argv[1][i] != ' ' && argv[1][i] != '\0' && argv[1][i] != '\t')
+		{
+			write (1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
