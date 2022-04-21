f01590c9a2fb4dde04e2c0e8a492a9181091ac75
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-04-21 12:35:09 +0930

    Update first_word.c

--- level01/first_word/first_word.c
+++ level01/first_word/first_word.c
@@ -8,8 +8,6 @@ int		main(int argc, char **argv)
 	i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
-			i++;
 		while (argv[1][i] != ' ' && argv[1][i] != '\0' && argv[1][i] != '\t')
 		{
 			write (1, &argv[1][i], 1);
