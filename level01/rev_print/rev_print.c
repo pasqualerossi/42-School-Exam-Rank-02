e6b6ec3ebbc38f07f23fed91066d23520b70d399
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-03 15:04:11 +0930

    Update rev_print.c

--- level01/rev_print/rev_print.c
+++ level01/rev_print/rev_print.c
@@ -1,19 +1,15 @@
 #include <unistd.h>
 
-int		main(int	argc, char **argv)
+int	main(int argc, char *argv[])
 {
 	int	i = 0;
 
 	if (argc == 2)
 	{
-		while (argv[1][i] != '\0')
-			i++;
-		i--;
-		while (i >= 0)
-		{
-			write(1, &argv[1][i], 1);
-			i--;
-		}
+		while (argv[1][i])
+			i += 1;
+		while (i)
+			write(1, &argv[1][--i], 1);
 	}
 	write(1, "\n", 1);
 	return (0);
