853a7d30f5e1d0ec58aa076b814bc14aea322b0e
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-03 15:57:38 +0930

    Update rot_13.c

--- level01/rot_13/rot_13.c
+++ level01/rot_13/rot_13.c
@@ -1,25 +1,21 @@
 #include <unistd.h>
 
-int		main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int	i = 0;
-	
-	if (argc == 2)
+	int i = 0;
+
+	if (argc > 1)
 	{
-		while (argv[1][i] != '\0')
-		{	
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'M')
-				argv[1][i] += 13;
-			else if (argv[1][i] >= 'N' && argv[1][i] <= 'Z')
-				argv[1][i] -= 13;
-			else if (argv[1][i] >= 'a' && argv[1][i] <= 'm')
-				argv[1][i] += 13;
-			else if (argv[1][i] >= 'n' && argv[1][i] <= 'Z')
-				argv[1][i] -= 13;
+		while(argv[1][i])
+		{
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				argv[1][i] = (argv[1][i] - 'a' + 13) % 26 + 'a';
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+					av[1][i] = (argv[1][i] - 'A' + 13) % 26 + 'A';
 			write(1, &argv[1][i], 1);
 			i++;
 		}
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
