30732d5bdc57d3cdf45f01a3cf6d6d8a42d56a0c
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-17 12:47:23 +0930

    Update first_word.c

--- level01/first_word/first_word.c
+++ level01/first_word/first_word.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int		main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-	int	i = 0;
-	
+	int i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] != ' ' && argv[1][i] != '\0' && argv[1][i] != '\t')
+		while (argv[1][i] == 32 || argv[1][i] == 9)
+			i++;
+		while ((argv[1][i] != 32 && argv[1][i] != 9) && argv[1][i])
 		{
-			write (1, &argv[1][i], 1);
+			write(1, &argv[1][i], 1);
 			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
 }
