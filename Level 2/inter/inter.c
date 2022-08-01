3cab02bc4158b111ce09dbf41c87fb3264ce77a6
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:43:37 +0930

    Update inter.c

--- Level 2/inter/inter.c
+++ Level 2/inter/inter.c
@@ -1,45 +1,29 @@
 #include <unistd.h>
 
-int     check_doubles(char  *str, char  c, int  position)
+int	iter(char *str, char c, int len)
 {
-    int     i;
+	int	i = 0;
 
-    i = 0;
-    while (i < position)
-    {
-        if (str[i] == c)
-            return (0);
-        i++;
-    }
-    return (1);
+	while (str[i] && (i < len || len == -1))
+		if (str[i++] == c)
+			return (1);
+	return (0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
-    int i;
-    int j;
+	int	i;
 
-    i = 0;
-    if (argc == 3)
-    {   
-        while (argv[1][i] != '\0')
-        {
-            j = 0;
-            while (argv[2][j] != '\0')
-            {
-                if (argv[1][i] == argv[2][j])
-                {
-                    if (check_doubles(argv[1], argv[1][i], i))
-                    {
-                        write(1, &argv[1][i], 1);
-                        break ;
-                    }    
-                }
-                j++;
-            }
-            i++;
-        }
-    }
-    write(1, "\n", 1);
-    return (0);
+	if (argc == 3)
+	{
+		i = 0;
+		while (argv[1][i])
+		{
+			if (!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
+				write(1, &argv[1][i], 1);
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
