d7eae73085fa3e736a8889e42b2484d25a2b3467
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 14:19:59 +0930

    Update atoi_base.c

--- Level 3/ft_atoi_base/atoi_base.c
+++ Level 3/ft_atoi_base/atoi_base.c
@@ -1,30 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   atoi_base.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/17 17:43:40 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/01/17 17:48:08 by mbarutel         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-int	atoi_base(const char *str, int str_base)
+int ft_isblank(char c)
 {
-    int     ret;
-    int     sign;
-    char    *char_input;
-    
-    ret = 0;
-    sign = 1;
-    char_input = "0123456789abcdef";
-    if (str[0] == '-' || str[0] == '+')
-    {
-        if (str[0] == '-')
-            sign = -1;
-        str++;
-    }
-    while (str[0] != '\0')
-    
+	if (c <= 32)
+		return (1);
+	return (0);
+}
+
+int		ft_isvalid(char c, int base)
+{
+	char digits[17] = "0123456789abcdef";
+	char digits2[17] = "0123456789ABCDEF";
+
+	while (base--)
+		if (digits[base] == c || digits2[base] == c)
+			return (1);
+	return (0);
+}
+
+int		ft_value_of(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f')
+		return (c - 'a' + 10);
+	else if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	return (0);
+}
+
+int		ft_atoi_base(const char *str, int str_base)
+{
+	int result;
+	int sign;
+
+	result = 0;
+	while (ft_isblank(*str))
+		str++;
+	sign = (*str == '-') ? -1 : 1;
+	(*str == '-' || *str == '+') ? ++str : 0;
+	while (ft_isvalid(*str, str_base))
+		result = result * str_base + ft_value_of(*str++);
+	return (result * sign);
 }
