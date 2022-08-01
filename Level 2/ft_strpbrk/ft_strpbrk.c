a0eb106d5fd7f7824bd03f30b7f03c06cc4be84f
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:39:15 +0930

    Update ft_strpbrk.c

--- Level 2/ft_strpbrk/ft_strpbrk.c
+++ Level 2/ft_strpbrk/ft_strpbrk.c
@@ -1,40 +1,20 @@
-// Passed Moulinette 2019.09.02
-
-int		find_char(const char c, const char *str)
+char	*ft_strchr(const char *s, int c)
 {
-	while (*str)
-	{
-		if (*str == c)
-			return (1);
-		++str;
-	}
-	return (0);
+	if (*s == c)
+		return ((char *)s);
+	else if (!*s)
+		return (NULL);
+	else
+		return (ft_strchr(s + 1, c));
 }
 
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
 	while (*s1)
 	{
-		if (find_char(*s1, s2) == 1)
-			return ((char *)s1);
-		++s1;
+		if (ft_strchr(s2, *s1))
+			return (char *)s1;
+		s1++;
 	}
-	return (0);
+	return (NULL);
 }
-
-//--------------------------------------------------------------
-// #include <stdio.h>
-// #include <string.h>
-
-// int		main(void)
-// {
-// 	printf("ft: %s; real: %s\n", \
-// 		ft_strpbrk("This is a string to search in", "qxd"), \
-// 		strpbrk("This is a string to search in", "qxd")
-// 		);
-
-// 	printf("ft: %s; real: %s\n", \
-// 		ft_strpbrk("This is a string to search in", "sh"), \
-// 		strpbrk("This is a string to search in", "sh")
-// 		);
-// }
