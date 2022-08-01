6efbe6973402e991ee460bcdf01d431931f9af08
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:19:54 +0930

    Update alpha_mirror.c

--- Level 2/alpha_mirror/alpha_mirror.c
+++ Level 2/alpha_mirror/alpha_mirror.c
@@ -1,19 +1,21 @@
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
+	char	ltr;
 	int	i = 0;
-	
+
 	if (argc == 2)
 	{
-		while (argv[1][i] != '\0')
+		while (argv[1][i])
 		{
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				argv[1][i] = 'M' - (argv[1][i] - 'N');
-			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-				argv[1][i] = 'm' - (argv[1][i] - 'n');
-			write(1, &argv[1][i], 1);
-			i++;
+			ltr = argv[1][i];
+			if ('A' <= argv [1][i] && 'Z' >= argv[1][i])
+				ltr = 'Z' - argv[1][i] + 'A';
+			if ('a' <= argv[1][i] && 'z' >= argv[1][i])
+				ltr = 'z' -argv[1][i] + 'a';
+			write(1, &ltr, 1);
+			i += 1;
 		}
 	}
 	write(1, "\n", 1);
