113da50e5302cf6139b01425a0025dffa7ba3489
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 14:29:56 +0930

    Update paramsum.c

--- Level 3/paramsum/paramsum.c
+++ Level 3/paramsum/paramsum.c
@@ -1,5 +1,3 @@
-// Passed Moulinette 2019.09.01
-
 #include <unistd.h>
 
 void	ft_putnbr(int n)
@@ -8,17 +6,15 @@ void	ft_putnbr(int n)
 
 	if (n >= 10)
 		ft_putnbr(n / 10);
-
 	digit = (n % 10) + '0';
 	write(1, &digit, 1);
 }
 
 int		main(int argc, char **argv)
 {
-	(void)argv;		// Silence 'unused parameter' error
+	(void)argv;
 
 	ft_putnbr(argc - 1);
 	write(1, "\n", 1);
-
 	return (0);
 }
