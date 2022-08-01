68c388d7d4b1f4b9d60f27202f63d94519ac49eb
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:54:03 +0930

    Update union.c

--- Level 2/union/union.c
+++ Level 2/union/union.c
@@ -1,37 +1,32 @@
 #include <unistd.h>
 
-void	union(char *a, char *b)
+int	not_seen_before(char *s, int max_pos, char c)
 {
-	int characters[128] = {0};
-	int c;
+	int	i = -1;
 
-	while (*a != '\0')
-	{
-		c = *a;
-		if (characters[c] == 0)
-		{
-			write(1, a, 1);
-			characters[c] = 1;
-		}
-		++a;
-	}
-
-	while (*b != '\0')
-	{
-		c = *b;
-		if (characters[c] == 0)
-		{
-			write(1, b, 1);
-			characters[c] = 1;
-		}
-		++b;
-	}
+	while(++i < max_pos)
+		if (s[i] == c)
+			return (0);
+	return (1);
 }
 
-int		main(int argc, char **argv)
+void	ft_union(char *s1, char *s2)
+{
+	int	i = -1;
+	int	j = -1;
+
+	while (s1[++i])
+		if (not_seen_before(s1, i, s1[i]))
+			write(1, &s1[i], 1);
+	while (s2[++j])
+		if (not_seen_before(s1, i, s2[j]) & not_seen_before(s2, j, s2[j]))
+			write(1, &s2[j], 1);
+}
+
+int	main(int argc, char **argv)
 {
 	if (argc == 3)
-		union(argv[1], argv[2]);
+		ft_union(argv[1], argv[2]);
 	write(1, "\n", 1);
 	return (0);
 }
