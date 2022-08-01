f0a0546b96f6572d4b2a7c268bd89e1389e44e54
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:49:34 +0930

    Update reverse_bits.c

--- Level 2/reverse_bits/reverse_bits.c
+++ Level 2/reverse_bits/reverse_bits.c
@@ -1,16 +1,13 @@
-// Passed Moulinette 2019.09.01
-
 unsigned char	reverse_bits(unsigned char octet)
 {
-	unsigned char out = 0;
+	int	i = 8;
+	unsigned char	res = 0;
 
-	out = out | ((octet & 128) >> 7);
-	out = out | ((octet & 64) >> 5);
-	out = out | ((octet & 32) >> 3);
-	out = out | ((octet & 16) >> 1);
-	out = out | ((octet & 8) << 1);
-	out = out | ((octet & 4) << 3);
-	out = out | ((octet & 2) << 5);
-	out = out | ((octet & 1) << 7);
-	return (out);
+	while (i > 0)
+	{
+		res = res * 2 + (octet % 2);
+		octet = octet / 2;
+		i--;
+	}
+	return (res);
 }
