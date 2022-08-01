ae74ffb6a82b342881b299f20e16e7aee09ba93e
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:45:12 +0930

    Update last_word.c

--- Level 2/last_word/last_word.c
+++ Level 2/last_word/last_word.c
@@ -1,23 +1,27 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+void	last_word(char *str)
 {
-    int     i = 0;
-    
-    if (argc == 2)
-    {
-        while (argv[1][i] != '\0')
-            i++;
-        i--;
-        while ((argv[1][i] != ' ' && argv[1][i] != '\t') && i >= 0)
-            i--;
-        i++;
-        while (argv[1][i] != '\0')
-        {
-            write(1, &argv[1][i], 1);
-            i++;
-        }
-    }
-    write(1, "\n", 1);
-    return (0);
+	int	j = 0;
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] >= 33 && str[i + 1] <= 126)
+			j = i + 1;
+		i++;
+	}
+	while (str[j] >= 33 && str[j] <= 127)
+	{
+		write(1, &str[j], 1);
+		j++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+		last_word(argv[1]);
+	write(1, "\n", 1);
+	return (0);
 }
