6c190e8b15a01c2e9017ebd6b43168947f369867
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 14:17:11 +0930

    Update expand_str.c

--- Level 3/expand_str/expand_str.c
+++ Level 3/expand_str/expand_str.c
@@ -1,38 +1,29 @@
-// Passed Moulinette 2019.09.02
-
 #include <unistd.h>
 
-int		word_len(char *str)
+int main(int argc, char const *argv[])
 {
-	int i = 0;
-	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t')
-		++i;
-	return (i);
-}
+    int i;
+    int flag;
 
-void	expand_str(char *str)
-{
-	int len;
-	int first_word = 1;
-
-	while (*str != '\0')
-	{
-		while (*str == ' ' || *str == '\t')
-			++str;
-		len = word_len(str);
-		if (len > 0 && first_word == 0)
-			write(1, "   ", 3);
-		first_word = 0;
-		write(1, str, len);
-		str = str + len;
-	}
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc == 2)
-		expand_str(argv[1]);
-
-	write(1, "\n", 1);
-	return (0);
+    if (argc == 2)
+    {
+        i = 0;
+        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+            i++;
+        while (argv[1][i])
+        {
+            if (argv[1][i] == ' ' || argv[1][i] == '\t')
+                flag = 1;
+            if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+            {
+                if (flag)
+                    write(1, "   ", 3);
+                flag = 0;
+                write(1, &argv[1][i], 1);
+            }
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
 }
