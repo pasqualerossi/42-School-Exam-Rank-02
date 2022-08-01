371bd783afcdd26229685361171ffb73e7c2365a
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:33:01 +0930

    Update ft_atoi.c

--- Level 2/ft_atoi/ft_atoi.c
+++ Level 2/ft_atoi/ft_atoi.c
@@ -4,15 +4,10 @@ int	ft_atoi(char *str)
 	int sign = 1;
 
 	while (*str == ' ' || (*str >= 9 && *str <= 13))
-    {	
-        str++;
-    }
+        	str++;
 	if (*str == '-')
-    {
 		sign = -1;
-    }
 	if (*str == '-' || *str == '+')
-    {
 		str++;
 	while (*str >= '0' && *str <= '9')
 	{
