fec88cc2f77a48b8aadfe9bde8bc27564b9d7ff5
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:33:52 +0930

    Update ft_strcmp.c

--- Level 2/ft_strcmp/ft_strcmp.c
+++ Level 2/ft_strcmp/ft_strcmp.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-
-int     ft_strcmp(char  *s1, char   *s2)
+int ft_strcmp(char *s1, char *s2)
 {
-    int     i = 0;
-    
-    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-        i++;
-    return(s1[i] - s2[i]);
+	while (*s1++ == *s2++)
+		if (!*s1 && !*s2)
+			return (0);
+	return (*--s1 - *--s2);
 }
