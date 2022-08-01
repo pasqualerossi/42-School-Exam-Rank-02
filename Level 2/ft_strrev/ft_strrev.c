7e67fe88a9d0abd23399d8d6939b30b7261a9404
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-01 12:41:00 +0930

    Update ft_strrev.c

--- Level 2/ft_strrev/ft_strrev.c
+++ Level 2/ft_strrev/ft_strrev.c
@@ -1,29 +1,16 @@
-int     ft_strlen(char *str)
+char *ft_strrev(char *str)
 {
-    int i;
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
+	int i = -1;
+	int len = 0;
+	char tmp;
 
-char    *ft_strrev(char *str)
-{
-    int     i;
-    char    temp;
-    int     len;
-
-    i = 0;
-    temp = 0;
-    len = ft_strlen(str);
-    len--;
-    while (len > i)
-    {
-        temp = str[i];
-        str[i] = str[len];
-        str[len] = temp;
-        i++;
-        len--;
-    }
-    return(str);
+	while (str[len])
+		len++;
+	while (++i < --len)
+	{
+		tmp = str[i];
+		str[i] = str[len];
+		str[len] = tmp;
+	}
+	return (str);
 }
