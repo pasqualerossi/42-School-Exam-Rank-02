ed87aa7fcfbaba96d13f80135a436357ac5f5139
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-02 14:42:35 +0930

    Update ft_split.c

--- Level 4/ft_split/ft_split.c
+++ Level 4/ft_split/ft_split.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 
-int		ft_wordlen(char *str)
+int	ft_wordlen(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
 		++i;
 	return (i);
@@ -13,8 +14,8 @@ char	*word_dupe(char *str)
 	int i = 0;
 	int len = ft_wordlen(str);
 	char *word = malloc(sizeof(char) * (len + 1));
+	
 	word[len] = '\0';
-
 	while (i < len)
 	{
 		word[i] = str[i];
@@ -26,6 +27,7 @@ char	*word_dupe(char *str)
 void	fill_words(char **array, char *str)
 {
 	int word_index = 0;
+	
 	while (*str == ' ' || *str == '\t' || *str == '\n')
 		++str;
 	while (*str != '\0')
@@ -42,6 +44,7 @@ void	fill_words(char **array, char *str)
 int		count_words(char *str)
 {
 	int num_words = 0;
+	
 	while (*str == ' ' || *str == '\t' || *str == '\n')
 		++str;
 	while (*str != '\0')
@@ -59,11 +62,11 @@ char	**ft_split(char *str)
 {
 	int		num_words;
 	char	**array;
-
+	
 	num_words = count_words(str);
 	array = malloc(sizeof(char *) * (num_words + 1));
+	
 	array[num_words] = 0;
-
 	fill_words(array, str);
 	return (array);
 }
