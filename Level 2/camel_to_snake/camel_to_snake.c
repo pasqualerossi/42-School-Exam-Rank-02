cca2ba2ee2520ea6b47d0e376040ec0ac64b1a63
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-04 13:02:26 +0930

    Update camel_to_snake.c

--- Level 2/camel_to_snake/camel_to_snake.c
+++ Level 2/camel_to_snake/camel_to_snake.c
@@ -1,24 +1,21 @@
 #include <unistd.h>
 
-void	camel_to_snake(char *str)
+int main(int argc, char **argv)
 {
-	while (*str != '\0')
-	{
-		if (*str >= 'A' && *str <= 'Z')
-		{
-			write(1, "_", 1);
-			*str = *str + ('a' - 'A');
-		}
-		write(1, str, 1);
-		++str;
-	}
-}
+	int i = 0;
 
-int		main(int argc, char **argv)
-{
 	if (argc == 2)
-		camel_to_snake(argv[1]);
-
-	write(1, "\n", 1);
-	return (0);
+	{
+		while(argv[1][i])
+		{
+			if(argv[1][i] >= 65 && argv[1][i] <= 90)
+			{
+				argv[1][i] = argv[1][i] + 32;
+				write (1, "_", 1);
+			}
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write (1, "\n", 1);
 }
