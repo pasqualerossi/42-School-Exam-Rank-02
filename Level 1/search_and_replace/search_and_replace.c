4f306554dcdc8c746302410c4b0fc69382ac86e8
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-04 12:59:17 +0930

    Update search_and_replace.c

--- Level 1/search_and_replace/search_and_replace.c
+++ Level 1/search_and_replace/search_and_replace.c
@@ -1,23 +1,20 @@
 #include <unistd.h>
 
-int		main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i = 0;
+    int i = 0;
 
-	if (argc == 4)
-	{
-		if (!argv[2][1] && !argv[3][1])
-		{
-			while (argv[1][i])
-			{
-				if (argv[1][i] == argv[2][0])
-					write(1, &argv[3][0], 1);
-				else
-					write(1, &argv[1][i], 1);
-				i += 1;
-			}
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+    if (argc == 4 && !argv[2][1] & !argv[3][1])
+    {
+        while (argv[1][i]!='\0')
+        {
+            if (argv[1][i]==argv[2][0])
+            {
+                argv[1][i] = argv[3][0];
+            }
+            write (1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write (1, "\n", 1);
 }
