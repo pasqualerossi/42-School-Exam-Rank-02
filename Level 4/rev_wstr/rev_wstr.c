1a06f032b0555f278ede3e31b2d5b3f411b16e09
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-18 17:06:24 +0930

    Update rev_wstr.c

--- Level 4/rev_wstr/rev_wstr.c
+++ Level 4/rev_wstr/rev_wstr.c
@@ -1,35 +1,35 @@
 #include <unistd.h>
-#include <stdlib.h>
 
-void	rev_wstr(char *str)
+int main(int argc, char **argv)
 {
+	int start;
+	int end;
 	int i = 0;
-	int j;
-	int first_word = 1;
-
-	while (str[i] != '\0')
-		++i;
-
-	while (i >= 0)
-	{
-		while (i >= 0 && (str[i] == '\0' || str[i] == ' ' || str[i] == '\t'))
-			--i;
-		j = i;
-		while (j >= 0 && str[j] != ' ' && str[j] != '\t')
-			--j;
-		if (first_word == 0)
-			write(1, " ", 1);
-		write(1, str + j + 1, i - j);
-		first_word = 0;
-		i = j;
+		
+	if(argc == 2)
+	{   
+		while(argv[1][i] != '\0')
+			i++;
+		while(i >= 0)
+		{
+			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+				i--;
+			end = i;
+			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+				i--;
+			start = i + 1;
+			int  flag;
+			flag = start;
+			while(start <= end)
+			{
+				write (1, &argv[1][start],1);
+				start++;		
+			}
+			if (flag !=0)
+			{
+				write(1, " ", 1);
+			}
+		}
 	}
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc == 2)
-		rev_wstr(argv[1]);
-
 	write(1, "\n", 1);
-	return (0);
 }
