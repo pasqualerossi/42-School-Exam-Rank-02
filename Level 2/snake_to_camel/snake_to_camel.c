94d3f38e6275768fcb294fb6e25ac7a220b2baee
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-18 17:00:41 +0930

    Update snake_to_camel.c

--- Level 2/snake_to_camel/snake_to_camel.c
+++ Level 2/snake_to_camel/snake_to_camel.c
@@ -1,26 +1,21 @@
-// Passed Moulinette 2019.09.02
-
 #include <unistd.h>
 
-void	snake_to_camel(char *str)
+int main(int argc, char **argv)
 {
-	while (*str != '\0')
+	int i = 0;
+	
+	if (argc ==2 )
 	{
-		if (*str == '_')
+		while(argv[1][i] != '\0')
 		{
-			++str;
-			*str = *str - ('a' - 'A');
+			if (argv[1][i] == '_')
+			{
+				i++;
+				argv[1][i] = argv[1][i]-32;
+			}
+			write (1, &argv[1][i], 1);
+			i++;
 		}
-		write(1, str, 1);
-		++str;
 	}
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-		snake_to_camel(argv[1]);
-
 	write(1, "\n", 1);
-	return (0);
 }
