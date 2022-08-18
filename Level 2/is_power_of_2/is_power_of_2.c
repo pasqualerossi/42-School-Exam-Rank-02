6e1c84d09369e436a733aaec2b8c575400e7c188
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-18 16:58:04 +0930

    Update is_power_of_2.c

--- Level 2/is_power_of_2/is_power_of_2.c
+++ Level 2/is_power_of_2/is_power_of_2.c
@@ -1,7 +1,14 @@
 int is_power_of_2(unsigned int n)
 {
-	if (n == 0)
-		return (0);
-	else
-		return ((n & (-n)) == n ? 1: 0);
+	int number = 1;
+	
+	while(number <= n)
+	{
+		if (number == n)
+		{
+			return 1;
+		}
+		number = number * 2;
+	}
+	return 0;
 }
