c2fe9995b21a6f7fd147358c88fbdd8143aacf9c
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-18 16:54:00 +0930

    Update ft_strpbrk.c

--- Level 2/ft_strpbrk/ft_strpbrk.c
+++ Level 2/ft_strpbrk/ft_strpbrk.c
@@ -1,20 +1,23 @@
-char	*ft_strchr(const char *s, int c)
-{
-	if (*s == c)
-		return ((char *)s);
-	else if (!*s)
-		return (NULL);
-	else
-		return (ft_strchr(s + 1, c));
-}
+#include <stdio.h>
+#include <string.h>
 
-char	*ft_strpbrk(const char *s1, const char *s2)
+char *ft_strpbrk(const char *s1, const char *s2)
 {
-	while (*s1)
+	int i = 0;
+	
+	if (!s1 || !s2)
 	{
-		if (ft_strchr(s2, *s1))
-			return (char *)s1;
-		s1++;
+		return (0);
 	}
-	return (NULL);
+	while(*s1)
+	{
+	   	while(s2[i])
+		{
+			if(*s1 == s2[i])
+				return (char *) s1;
+			i++;
+		}
+		s1++;	
+	}
+	return (0);
 }
