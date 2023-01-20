26c085c748679b4eef1fc6e35d0e0465cfc1b332
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-01-20 17:14:26 +1030

    Update ft_itoa.c

--- Level 4/ft_itoa/ft_itoa.c
+++ Level 4/ft_itoa/ft_itoa.c
@@ -1,39 +1,44 @@
 #include <stdlib.h>
 
-char    *ft_itoa(int nbr)
+static unsigned int	ft_number_size(int number)
 {
-	int	len;
-	long	n_tmp;
-	char	*str;
-
-	len = 0;
-	n_tmp = nbr;
-	str[len] ='\0';
+	unsigned int length = 0;
 	
-	if (nbr == -2147483648)
-		return ("-2147483648");
-	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
+	if (number == 0)
+		return (1);
+	if (number < 0)
+		length += 1;
+	while (number != 0)
+	{
+		number /= 10;
+		length++;
+	}
+	return (length);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*string = (char *)malloc(sizeof(char) * (length + 1));
+	unsigned int	number;
+	unsigned int	length = ft_number_size(n);
+
+	if (string == NULL)
 		return (NULL);
-	if (nbr == 0)
+	if (number < 0)
 	{
-		str[0] = '0';
-		return (str);
+		string[0] = '-';
+		number = -n;
 	}
-	if (nbr < 0)
+	else
+		number = n;
+	if (number == 0)
+		string[0] = '0';
+	string[length] = '\0';
+	while (number != 0)
 	{
-		len += 1;
-		nbr *= -1;
-		str[0] = '-';
+		string[length - 1] = (number % 10) + '0';
+		number = number / 10;
+		length--;
 	}
-	while (n_tmp)
-	{
-		n_tmp /= 10;
-		len += 1;
-	}
-	while (nbr)
-	{
-		str[--len] = (nbr % 10) + '0';
-		nbr /= 10;
-	}
-	return (str);
+	return (string);
 }
