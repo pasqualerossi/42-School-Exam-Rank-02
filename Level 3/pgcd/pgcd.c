7021187d3258ec8e9c8cbb69f9640c8846916b3c
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-01-23 15:56:43 +1030

    Update pgcd.c

--- Level 3/pgcd/pgcd.c
+++ Level 3/pgcd/pgcd.c
@@ -3,23 +3,21 @@
 
 int main(int argc, char const *argv[])
 {
-	int nbr1;
-	int nbr2;
+	int number1 = atoi(argv[1]);
+	int number2 = atoi(argv[2]);
 
-	if (argc == 3)
+	if (argc != 3 && number1 <=0 || number2 <= 0)
 	{
-		if ((nbr1 = atoi(argv[1])) > 0 && (nbr2 = atoi(argv[2])) > 0)
-		{
-			while (nbr1 != nbr2)
-			{
-				if (nbr1 > nbr2)
-					nbr1 -= nbr2;
-				else
-					nbr2 -= nbr1;
-			}
-			printf("%d", nbr1);
-		}
+		printf("\n");
+		return 0;
 	}
-	printf("\n");
-	return (0);
+	while (number2 != 0)
+	{
+		//ft_swap
+		int temporary = number2;
+		number2 = number1 % number2;
+		number1 = temporary;
+	}
+	printf("%d\n", number1);
+	return 0;
 }
