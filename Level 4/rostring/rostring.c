124ac8cf7d0d48b969690240406e425998a60e58
Fernando <96313626+nands93@users.noreply.github.com>
2023-02-01 11:24:06 -0300

    Update rostring.c
    Used to fail in the second exemple, where you have more than one consecutive space or tab. Now should work.

--- Level 4/rostring/rostring.c
+++ Level 4/rostring/rostring.c
@@ -2,36 +2,39 @@
 
 int main(int argc, char **argv)
 {
-	int i = 0;
-	int start;
-	int end;
-	int flag = 0;
-	
-	if (argc > 1)
-	{
-		while(argv[1][i] == ' ' || argv[1][i] == '\t')
-			i++;
-		start = i;
-		while(argv[1][i] && (argv[1][i] != ' ' &&  argv[1][i] != '\t'))
-			i++;
-		end = i;
-		while(argv[1][i] == ' ' || argv[1][i] == '\t')
-			i++;
-		while(argv[1][i] != '\0')
-		{
-			flag = 1;
-			write (1, &argv[1][i], 1);
-			i++;				
-		}
-		if (flag == 1)
-		{
-			write (1, " ", 1);
-		}
-		while(start < end)
-		{
-			write (1, &argv[1][start], 1);
-			start++;
-		}
-	}	
-	write (1, "\n",1);	
+    int i;
+    int start;
+    int end;
+    int flag;
+
+    if (argc > 1)
+    {
+        i = 0;
+        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+            i++;
+        start = i;
+        while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
+            i++;
+        end = i;
+        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+            i++;
+        while(argv[1][i]) 
+        {
+            while ((argv[1][i] == ' ' && argv[1][i + 1] == ' ') || (argv[1][i] == '\t' && argv[1][i + 1] == '\t'))
+                i++; 
+            if (argv[1][i] == ' ' || argv[1][i] == '\t')
+                flag = 1; 
+            write(1, &argv[1][i], 1);
+            i++;
+        }
+        if (flag)
+            write(1, " ", 1);
+        while (start < end)
+        {
+            write(1, &argv[1][start], 1);
+            start++;
+        }
+    }
+    write(1, "\n", 1);
+    return(0);
 }
