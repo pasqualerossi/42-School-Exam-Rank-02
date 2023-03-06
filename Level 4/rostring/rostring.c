beb82504c93dfa5e43b532daf89d03c94c435bb5
markjso <97271827+markjso@users.noreply.github.com>
2023-03-06 12:26:12 +1030

    Update rostring.c
    This was segfaulting on the 4th example but works now.

--- Level 4/rostring/rostring.c
+++ Level 4/rostring/rostring.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
     int end;
     int flag;
 
-    if (argc > 1)
+    if (argc > 1 && av[1][0])
     {
         i = 0;
         while (argv[1][i] == ' ' || argv[1][i] == '\t')
