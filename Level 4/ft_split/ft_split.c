344678e4b3acdc04b4fb0af4f1613d519ffb3bd1
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-03-08 12:49:59 +1030

    Update ft_split.c

--- Level 4/ft_split/ft_split.c
+++ Level 4/ft_split/ft_split.c
@@ -1,60 +1,93 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-char **ft_split(char *str) 
+int	ft_count_str(char const *s)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int word_count = 0;
-    int len = strlen(str);
+	int	i;
+	int	count;
 
-    while (i < len) 
-    {
-        while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')) 
+	i = 0;
+	count = 0;
+	if (!s[i])
+		return (0);
+	if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		i++;
+	while (s[i])
 	{
-            i++;
-        }
-        if (str[i]) 
+		if ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n') && (s[i - 1] != ' ' && s[i - 1] != '\t' && s[i - 1] != '\n'))
+			count++;
+		i++;
+	}
+	if (s[i] == '\0' && (s[i - 1] != ' ' && s[i - 1] != '\t' && s[i - 1] != '\n'))
+		count++;
+	return (count);
+}
+
+int	ft_str_len(char const *s, int i)
+{
+	int	len;
+
+	len = 0;
+	while (s[i] && (s[i] != ' ' && s[i] != '\t' && s[i] != '\n'))
 	{
-            word_count++;
-            while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')) 
-	    {
-                i++;
-            }
-        }
-    }
-    char **result = (char**)malloc(sizeof(char*) * (word_count + 1));
-    if (result == NULL) 
-    {
-        return NULL;
-    }
-    i = 0;
-    while (i < len) 
-    {
-        while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')) 
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+char	*ft_write_string(char const *s, char **ptr, int j, int len)
+{
+	int		x;
+	char	*p;
+
+	x = 0;
+	p = (char *)s;
+	ptr[j] = malloc((len + 1) * sizeof(char));
+	if (!ptr[j])
 	{
-            i++;
-        }
-        if (str[i]) 
+		while (j != 0)
+		{
+			free(ptr[j]);
+			j--;
+		}
+		free(ptr);
+		return (NULL);
+	}
+	while (len != 0)
 	{
-            j = i;
-            while (str[j] && (str[j] != ' ' && str[j] != '\t' && str[j] != '\n')) 
-	    {
-                j++;
-            }
-            result[k] = (char*)malloc(sizeof(char) * (j - i + 1));
-            if (result[k] == NULL) 
-	    {
-                return NULL;
-            }
-            strncpy(result[k], str + i, j - i);
-            result[k][j - i] = '\0';
-            k++;
-            i = j;
-        }
-    }
-    result[k] = NULL;
-    return result;
+		ptr[j][x] = p[x];
+		x++;
+		len--;
+	}
+	ptr[j][x] = '\0';
+	return (ptr[j]);
+}
+
+char	**ft_split(char const *s)
+{
+	char	**ptr;
+	int		i;
+	int		j;
+	int		len;
+
+	i = 0;
+	j = 0;
+	ptr = malloc((ft_count_str(s) + 1) * sizeof(char *));
+	if (!ptr)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
+		{
+			len = ft_str_len(s, i);
+			ptr[j] = ft_write_string(s + i, ptr, j, len);
+			j++;
+			i += len;
+			if (s[i] == '\0')
+				break ;
+		}
+		i++;
+	}
+	ptr[j] = NULL;
+	return (ptr);
 }
