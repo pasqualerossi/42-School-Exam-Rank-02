88a283310a445e92bddb3ce278d38e8c79106ee1
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-05-29 20:39:59 +0930

    Update ft_range.c

--- Level 3/ft_range/ft_range.c
+++ Level 3/ft_range/ft_range.c
@@ -2,24 +2,24 @@
 
 int *ft_range(int start, int end)
 {
-   int i = 0;
-   int len = abs((end - start)) + 1;
-   int *res = (int *)malloc(sizeof(int) * len);
-
-   while (i < len)
-   {
-       if (start < end)
-       {
-	  res[i] = start;
-	  start++;
-		  i++;
-       }
-       else
-       {
-	 res[i] = start;
-	 start--;
-	 i++;
-       }
-   }
+	int i = 0;
+	int len = abs((end - start)) + 1;
+	int *res = (int *)malloc(sizeof(int) * len);
+	
+	while (i < len)
+	{
+		if (start < end)
+		{
+			res[i] = start;
+			start++;
+			i++;
+		}
+		else
+		{
+			res[i] = start;
+			start--;
+			i++;
+		}
+	}
    return (res);
 }
