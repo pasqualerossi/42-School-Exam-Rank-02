e73ad8967e46367f16dd00ae4d81cc5375d84e8d
Clara Franco <131652967+clima-fr@users.noreply.github.com>
2023-06-06 18:08:30 +0100

    Update ft_itoa.c

--- Level 4/ft_itoa/ft_itoa.c
+++ Level 4/ft_itoa/ft_itoa.c
@@ -3,38 +3,37 @@
 
 char *ft_itoa(int nbr) 
 {
-    int n = nbr;
-    int len = 0;
-
-    if (nbr <= 0)
-    {
-	len++;
-    }
-    while (n) 
-    {
-        n /= 10;
-        len++;
-    }
-    char *result = (char *)malloc(sizeof(char) * (len + 1));
-    if (result == NULL) 
-    {
-        return NULL;
-    }
-    result[len] = '\0';
-    if (nbr == 0)
-    {
-	result[0] = '0';
-	return(result);
-    }
-    if (nbr < 0) 
-    {
-        result[0] = '-';
-        nbr = -nbr;
-    }
-    while (nbr) 
-    {
-        result[--len] = nbr % 10 + '0';
-        nbr /= 10;
-    }
-    return result;
+	if(nbr == -2147483648)
+		return("-2147483648\0");
+	int n = nbr;
+	int len = 0;
+	if (nbr <= 0)
+	{
+		len++;
+    	}
+	while (n) 
+	{
+		n /= 10;
+		len++;
+	}
+	char *result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL) 
+		return NULL;
+	result[len] = '\0';
+	if (nbr == 0)
+	{
+		result[0] = '0';
+		return(result);
+	}
+	if (nbr < 0) 
+	{
+		result[0] = '-';
+		nbr = -nbr;
+	}
+	while (nbr) 
+	{
+		result[--len] = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	return result;
 }
