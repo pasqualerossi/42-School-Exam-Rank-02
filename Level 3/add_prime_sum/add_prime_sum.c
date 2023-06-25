c321318e64a4b733d7da10102a7ad1a1fca235d9
Valerio D'Ambrosio <valeriodambrosio.42@proton.me>
2023-06-25 13:03:19 +0200

    Fix is_prime function and improve readability

--- Level 3/add_prime_sum/add_prime_sum.c
+++ Level 3/add_prime_sum/add_prime_sum.c
@@ -1,36 +1,53 @@
 #include <unistd.h>
 
-int is_prime(int num)
+int	ft_atoi(char *s)
 {
-	int i = 3;
-	
-	if (num <= 1)
-		return (0);
-	if (num % 2 == 0 && num > 2)
-		return (0);
-	while (i < (num / 2))
-	{
-		if (num % i == 0)
-			return 0;
-		i += 2;
-	}
-	return 1;
+	int	res = 0;
+
+	while (*s)
+		res = res * 10 + *s++ - 48;
+	return (res);
 }
 
-int main(int argc, char *argv[])
+int	is_prime(int num)
 {
-	int sum = 0;
-	int nb = ft_atoi(argv[1]);
-	
-	if (argc == 2)
+	int	i = 2;
+
+	if (num <= 1)
+		return (0);
+	while (i * i <= num)
 	{
-		while (nb > 0)
-			if (is_prime(nb--))
-				sum += (nb + 1);
-		ft_putnbr(sum);
+		if (num % i == 0)
+			return (0);
+		i++;
 	}
-	if (argc != 2)
-		ft_putnbr(0);
+	return (1);
+}
+
+void	put_nbr(int n)
+{
+	if (n >= 10)
+		put_nbr(n / 10);
+	char digit = n % 10 + '0';
+	write(1, &digit, 1);
+}
+
+int main(int ac, char **av)
+{
+
+	if (ac == 2)
+	{
+		int	nbr = ft_atoi(av[1]);
+		int	sum = 0;
+
+		while (nbr > 0)
+		{
+			if (is_prime(nbr))
+				sum += nbr;
+			nbr--;
+		}
+		put_nbr(sum);
+	}
 	write(1, "\n", 1);
 	return (0);
 }
