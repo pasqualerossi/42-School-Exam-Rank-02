c9bc2bc17d919555c20b9feffcef4b51e5bd82b8
David <34965385+Lartaxx@users.noreply.github.com>
2023-07-03 15:06:41 +0200

    Change unsigned int to lambda int
    When compiling without flags, no error appears, only with "-Wall -Werror" flags in the loop, an int is compared with an unsigned int, and the function doesn't compile.

--- Level 2/max/max.c
+++ Level 2/max/max.c
@@ -1,19 +1,15 @@
-int max(int *tab, unsigned int len)
+int     max(int *tab, unsigned int len)
 {
-	if (len == 0)
-		return (0);
-	
-	unsigned int result;
-	unsigned int i = 0;
-	
-	result = tab[i];
-	while(i < len)
-	{
-		if (result <  tab[i])
-		{
-			result = tab[i];
-		}
-		i++;
-	}
-	return result;
+    unsigned int i = 0;
+    int result = tab[i];
+
+    if (len == 0)
+        return (0);
+    while (i < len)
+    {
+        if (result < tab[i])
+            result = tab[i];
+        i++;
+    }
+    return (result);
 }
