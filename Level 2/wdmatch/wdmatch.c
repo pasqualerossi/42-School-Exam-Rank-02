03efb4a8bf704b634d94f606501768aa7b8b87c8
Loredana <20396933+LoredanaLoSurdo@users.noreply.github.com>
2024-04-16 11:23:23 +0930

    Update wdmatch.c
    updated i to reset and j to increment as certain inputs would result in the string in argument 1 to be printed multiple times

--- Level 2/wdmatch/wdmatch.c
+++ Level 2/wdmatch/wdmatch.c
@@ -3,24 +3,32 @@
 void ft_putstr(char const *str)
 {
 	int i = 0;
-
+	
 	while (str[i])
 		write(1, &str[i++], 1);
 }
 
-int	main(int argc, char const *argv[])
+int main(int ac, char **av)
 {
 	int i = 0;
 	int j = 0;
 
-	if (argc == 3)
+	if (ac == 3)
 	{
-		while (argv[2][j])
-			if (argv[2][j++] == argv[1][i])
-				i += 1;
-		if (!argv[1][i])
-			ft_putstr(argv[1]);
+		while (av[2][j])
+		{
+			if (av[2][j++] == av[1][i])
+			{
+				i++;
+			}
+			if (!av[1][i])
+			{
+				ft_putstr(av[1]);
+				i = 0;
+			}
+		}
+		j++;
 	}
 	write(1, "\n", 1);
-	return (0);
+	return 0;
 }
