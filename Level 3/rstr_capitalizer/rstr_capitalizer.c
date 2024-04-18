f3e0835f06664e2e863d889387e5ef27001fdb8d
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-04-18 17:50:17 +0930

    Update rstr_capitalizer.c

--- Level 3/rstr_capitalizer/rstr_capitalizer.c
+++ Level 3/rstr_capitalizer/rstr_capitalizer.c
@@ -8,8 +8,7 @@ void    rstr_capitalizer(char *str)
     {
         if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] += 32;
-        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
-                    || str[i + 1] == '\t' || str[i + 1] == '\0'))
+        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
             str[i] -= 32;
         write(1, &str[i++], 1);
     }
