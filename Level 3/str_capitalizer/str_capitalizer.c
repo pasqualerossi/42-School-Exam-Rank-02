874e4420290b0bbf8d053f720215d1142dd859b8
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-04-18 17:50:43 +0930

    Update str_capitalizer.c

--- Level 3/str_capitalizer/str_capitalizer.c
+++ Level 3/str_capitalizer/str_capitalizer.c
@@ -11,8 +11,7 @@ void	str_capitalizer(char *str)
 	{
 		if (str[i] >= 'A' && 'Z' >= str[i])
 			str[i] += 32;
-		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || \
-		str[i - 1] == '\t'))
+		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || str[i - 1] == '\t'))
 			str[i] -= 32;
 		write(1, &str[i], 1);
 	}
