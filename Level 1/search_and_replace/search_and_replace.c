dd2053fd08a1ed8b963748301085597f8ccdf1d1
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-04-18 17:45:21 +0930

    Update search_and_replace.c

--- Level 1/search_and_replace/search_and_replace.c
+++ Level 1/search_and_replace/search_and_replace.c
@@ -9,9 +9,7 @@ int main(int argc, char **argv)
         while (argv[1][i]!='\0')
         {
             if (argv[1][i]==argv[2][0])
-            {
                 argv[1][i] = argv[3][0];
-            }
             write (1, &argv[1][i], 1);
             i++;
         }
