7d7f79febe3d9563fe2a476e15c3ea3dbd74553d
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-04-18 17:41:06 +0930

    Update max.c

--- Level 2/max/max.c
+++ Level 2/max/max.c
@@ -1,18 +1,15 @@
 int max(int *tab, unsigned int len)
 {
-	if (len == 0)
-		return (0);
-	
 	int result;
 	unsigned int i = 0;
 	
+	if (len == 0)
+		return (0);
 	result = tab[i];
 	while(i < len)
 	{
 		if (result <  tab[i])
-		{
 			result = tab[i];
-		}
 		i++;
 	}
 	return result;
