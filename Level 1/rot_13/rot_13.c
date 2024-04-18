a9927fe11f3ec1a2307a71afb7d292c8803dda83
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-04-18 17:44:15 +0930

    Update rot_13.c

--- Level 1/rot_13/rot_13.c
+++ Level 1/rot_13/rot_13.c
@@ -11,7 +11,7 @@ int	main(int argc, char **argv)
 			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
 				argv[1][i] = (argv[1][i] - 'a' + 13) % 26 + 'a';
 			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-					argv[1][i] = (argv[1][i] - 'A' + 13) % 26 + 'A';
+				argv[1][i] = (argv[1][i] - 'A' + 13) % 26 + 'A';
 			write(1, &argv[1][i], 1);
 			i++;
 		}
