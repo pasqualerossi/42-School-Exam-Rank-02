3acc38f53d01fb0b59457774697c3bb39844f607
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-04-18 17:37:46 +0930

    Update ft_strcspn.c

--- Level 2/ft_strcspn/ft_strcspn.c
+++ Level 2/ft_strcspn/ft_strcspn.c
@@ -7,7 +7,7 @@ size_t  ft_strcspn(const char *s, const char *reject)
 
     while (s[i] != '\0')
     {
-		j = 0;
+	j = 0;
         while (reject[j] != '\0')
         {
             if(s[i] == reject[j])
