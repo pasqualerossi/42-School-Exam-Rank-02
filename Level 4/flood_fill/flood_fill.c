f10e8555a80e892683c6b5a223fecea55afa4e5b
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-04-18 17:52:09 +0930

    Update flood_fill.c

--- Level 4/flood_fill/flood_fill.c
+++ Level 4/flood_fill/flood_fill.c
@@ -1,15 +1,13 @@
 typedef struct	s_point
 {
-	int			x;
-	int			y;
-}				t_point;
+	int x;
+	int y;
+} t_point;
 
 void	fill(char **tab, t_point size, t_point cur, char to_fill)
 {
-	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x
-		|| tab[cur.y][cur.x] != to_fill)
+	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x || tab[cur.y][cur.x] != to_fill)
 		return;
-
 	tab[cur.y][cur.x] = 'F';
 	fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
 	fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
