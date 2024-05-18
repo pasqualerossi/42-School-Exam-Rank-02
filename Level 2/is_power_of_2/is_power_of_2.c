e74e6fdefa19b8408a55d108966dd90bce97b512
oprosvir <ol7897@ya.ru>
2024-05-19 00:52:21 +0200

    bitwise magic

--- Level 2/is_power_of_2/is_power_of_2.c
+++ Level 2/is_power_of_2/is_power_of_2.c
@@ -1,8 +1,6 @@
 int is_power_of_2(unsigned int n)
 {
-	if (n == 0)
-		return (0);
-	while (n % 2 == 0)
-		n /= 2;
-	return ((n == 1) ? 1 : 0);
+    if (n == 0)
+        return 0;
+    return (n & (n - 1)) == 0;
 }
