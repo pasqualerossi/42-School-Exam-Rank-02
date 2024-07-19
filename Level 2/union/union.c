940d25f9de93aea58e8c37a9263f91b06bb92f3a
Ani Mema <119635344+amema42@users.noreply.github.com>
2024-07-19 22:54:06 +0200

    update union.c
    clear! The int seen array keeps track of the characters that have already been encountered in both av[1] and av[2]. When a character from either string is encountered and printed, it is marked as seen in the array. So yeah, his prevents any duplicates from being printed

--- Level 2/union/union.c
+++ Level 2/union/union.c
@@ -1,46 +1,32 @@
-# include <unistd.h>
+#include <unistd.h>
 
-int has_char(char *str, char c)
+int main(int ac, char **av)
 {
-    while (*str)
+    if (ac == 3)
     {
-        if (*str == c)
-            return 1;
-        str++;
-    }
-    return 0;
-}
+        int seen[256] = {0}; /*The _seen_ array tracks characters encountered in both av[1] and av[2].*/
+        int i = 0;
+        int j = 0;
 
-void union_str(char *str1, char *str2)
-{
-    char seen[256] = {0};
-    int i = 0;
-
-    while (*str1)
-    {
-        if (!has_char(seen, *str1))
+        while (av[1][i])
         {
-            seen[i] = *str1;
-            write(1, str1, 1);
+            if (!seen[av[1][i]])
+            {
+                write(1, &av[1][i], 1);
+                seen[av[1][i]] = 1;
+            }
             i++;
         }
-        str1++;
-    }
-    while (*str2)
-    {
-        if (!has_char(seen, *str2))
+        while (av[2][j])
         {
-            seen[i] = *str2;
-            write(1, str2, 1);
-            i++;
+            if (!seen[av[2][j]])
+            {
+                write(1, &av[2][j], 1);
+                seen[av[2][j]] = 1;
+            }
+            j++;
         }
-        str2++;
     }
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc == 3)
-        union_str(argv[1], argv[2]);
     write(1, "\n", 1);
+    return 0;
 }
