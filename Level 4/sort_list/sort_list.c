51c21e020faa2f5df3c3d33b121373097fff7a32
Ani Mema <119635344+amema42@users.noreply.github.com>
2024-07-22 19:08:10 +0200

    update sort_list.c
    Update:
    1. Added the condition `(lst != NULL)` to the while loop to ensure that `lst` is not NULL.
    2. Renamed the variable from " *tmp " to " t_list *start " for clarity, as we are using this variable to store the start of the list.

--- Level 4/sort_list/sort_list.c
+++ Level 4/sort_list/sort_list.c
@@ -1,24 +1,23 @@
 #include <stdlib.h>
 #include "list.h"
 
-t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
-{
-	int	swap;
-	t_list	*tmp;
+t_list *sort_list(t_list* lst, int (*cmp)(int, int)) {
+    int swap;
+    t_list *start;
 
-	tmp = lst;
-	while(lst->next != NULL)
+    start = lst;
+
+    while (lst != NULL && lst->next != NULL)
+    {
+        if ((*cmp)(lst->data, lst->next->data) == 0)
 	{
-		if (((*cmp)(lst->data, lst->next->data)) == 0)
-		{
-			swap = lst->data;
-			lst->data = lst->next->data;
-			lst->next->data = swap;
-			lst = tmp;
-		}
-		else
-			lst = lst->next;
-	}
-	lst = tmp;
-	return (lst);
+            swap = lst->data;
+            lst->data = lst->next->data;
+            lst->next->data = swap;
+            lst = start;
+        }
+	else
+		lst = lst->next;
+    }
+    return (start);
 }
