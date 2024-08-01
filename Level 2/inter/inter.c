65b03e63a2fa0a7cf34f0a42348652bbc1d56173
Ani Mema <119635344+amema42@users.noreply.github.com>
2024-08-01 17:55:25 +0200

    update inter.c
    seen array declared as unsigned char

--- Level 2/inter/inter.c
+++ Level 2/inter/inter.c
@@ -1,23 +1,24 @@
 #include <unistd.h>
+
 int main(int ac, char **av)
 {
     int i;
     int j;
-    int seen[256] = {0};
+    unsigned char seen[256] = {0};
     if (ac == 3)
     {
         i = 0;
-        while(av[1][i])
+        while (av[1][i])
         {
             j = 0;
-            while(av[2][j])
+            while (av[2][j])
             {
                 if (av[1][i] == av[2][j])
                 {
-                    if (!seen[av[1][i]])
+                    if (!seen[(unsigned char)av[1][i]])
                     {
                         write(1, &av[1][i], 1);
-                        seen[av[1][i]] = 1;
+                        seen[(unsigned char)av[1][i]] = 1;
                     }
                     break;
                 }
