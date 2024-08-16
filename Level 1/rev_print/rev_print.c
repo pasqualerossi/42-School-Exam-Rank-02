cde77e836708a744914efca963d3d3335567f419
Hari <149360252+Harimino123@users.noreply.github.com>
2024-08-16 18:58:24 +0300

    Update rev_print.c
    For the code part after the last loop, I ran it through the moulinette by just doing write(1, &str[0], 1) without the if condition, but I think it’s a bit cleaner now because mine was outputting strange things if an empty argument was passed.

--- Level 1/rev_print/rev_print.c
+++ Level 1/rev_print/rev_print.c
@@ -2,13 +2,13 @@
 
 char *rev_print(char *str)
 {
-	int i = 0;
+	int	i = 0;
 
 	while (str[i])
 		i++;
 	while (--i > 0)
-		write (1, &str[i], 1);
+		write(1, &str[i], 1);
 	if (str[0])
-		write (1, &str[0], 1);
+		write(1, &str[0], 1);
 	return (str);
 }
