8a3221d7f631356d95500a88b19c53b6cf58128e
El-Pachris <81959047+elpah@users.noreply.github.com>
2024-08-31 06:25:43 +0200

    Update alpha_mirror.c
    Easier to understand the logic. 
    Basically adding a + b and subtracting letter gives the value.

--- Level 2/alpha_mirror/alpha_mirror.c
+++ Level 2/alpha_mirror/alpha_mirror.c
@@ -8,10 +8,10 @@ int main(int argc, char **argv)
 	{
 		while(argv[1][i]) 
 		{
-			if(argv[1][i] >= 65 && argv[1][i] <= 90)
-				argv[1][i] = 90 - argv[1][i] + 65;
-			else if (argv[1][i] >= 97 && argv[1][i] <= 122)
-				argv[1][i] = 122-argv[1][i] + 97;
+			if(argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				argv[1][i] = ('a' + 'z') - argv[1][i];
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				argv[1][i] = ('A' + 'Z') - argv[1][i];
 			write(1, &argv[1][i], 1);
 			i++;
 		}
