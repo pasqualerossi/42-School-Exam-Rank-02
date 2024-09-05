d37be8bc74009bc6a42e63941cb6b1850aa751ad
Rodrigo Monteiro <130557641+rode-lim@users.noreply.github.com>
2024-09-05 13:40:55 +0100

    Update rev_print.c

--- Level 1/rev_print/rev_print.c
+++ Level 1/rev_print/rev_print.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
 
-char *rev_print(char *str)
+char	*rev_print(char *str)
 {
 	int	i = 0;
-
-	while (str[i])
+	while(str[i])
 		i++;
-	while (--i > 0)
+	while(--i >= 0)
 		write(1, &str[i], 1);
-	if (str[0])
-		write(1, &str[0], 1);
+	write(1, "\n", 1);
 	return (str);
 }
