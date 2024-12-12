4d93ac57dff8a2b95d810edd1fdafd608829ee20
abdullah tursun <124070221+apotursun963@users.noreply.github.com>
2024-12-12 21:59:38 +0300

    Update rev_wstr.c

--- Level 4/rev_wstr/rev_wstr.c
+++ Level 4/rev_wstr/rev_wstr.c
@@ -1,33 +1,31 @@
+
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-	int start;
-	int end;
-	int i = 0;
-		
-	if(argc == 2)
-	{   
-		while(argv[1][i] != '\0')
-			i++;
-		while(i >= 0)
-		{
-			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
-				i--;
-			end = i;
-			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
-				i--;
-			start = i + 1;
-			int  flag;
-			flag = start;
-			while(start <= end)
-			{
-				write (1, &argv[1][start],1);
-				start++;		
-			}
-			if (flag !=0)
-				write(1, " ", 1);
-		}
-	}
-	write(1, "\n", 1);
+    int start, end;
+    int idx = 0;
+
+    if (argc == 2)
+    {
+        while (argv[1][idx])
+            idx++;
+        while (idx >= 0) 
+        {
+            while (idx >= 0 && (argv[1][idx] == ' ' || argv[1][idx] == '\t'))
+                idx--;
+            end = idx;
+            while (idx >= 0 && argv[1][idx] != ' ' && argv[1][idx] != '\t')
+                idx--;
+            start = idx + 1;
+            while (start <= end)
+            {
+                write(1, &argv[1][start], 1);
+                start++;
+            }
+            if (idx > 0)
+                write(1, " ", 1);
+        }
+    }
+    write(1, "\n", 1);
 }
